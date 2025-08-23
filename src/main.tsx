5ee28a4755b0a9d9cf656ab93403629a81b094c8
caharshalvjain <caharshalvjain@gmail.com>
2025-08-23 16:44:53 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ setTimeout(() => {
   // hide the 3-D pre-loader
   const loader = document.getElementById('loader-3d');
   if (loader) loader.classList.add('hide');
-
   // force the body to scroll
   document.body.style.overflow = 'auto';
-}, 2800); // match your loader fade-out time
+}, 2800);
+
